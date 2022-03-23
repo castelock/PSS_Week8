9cca5b11e547b2fa83953f730c91e893b3301eb9
juan jesus ojeda castelo <joc028@ual.es>
2022-03-23 19:48:21 +0100

    Agregar archivos de proyecto.

---
+++ Prototype/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Person : IPrototype<Person>
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+        public Person? clone()
+        {
+            return this.MemberwiseClone() as Person;
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+}
